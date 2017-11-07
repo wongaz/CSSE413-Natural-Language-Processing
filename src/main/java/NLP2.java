14649c78198e27ca8031c5a08f370f2ff53d07b7
Alex <wongaz@rose-hulman.edu>
2017-11-06 20:06:17 -0500

    jars away

--- src/main/java/NLP2.java
+++ src/main/java/NLP2.java
@@ -1,6 +1,10 @@
+import edu.stanford.nlp.ling.CoreAnnotations;
+import edu.stanford.nlp.ling.CoreLabel;
 import edu.stanford.nlp.pipeline.Annotation;
 import edu.stanford.nlp.pipeline.StanfordCoreNLP;
+import edu.stanford.nlp.util.CoreMap;
 
+import java.util.List;
 import java.util.Properties;
 
 public class NLP2 {
@@ -26,6 +30,22 @@ public class NLP2 {
         Annotation document = new Annotation(text);
         pipeline.annotate(document);
         System.out.println(text.toString());
+        List<CoreMap> sentences = document.get(CoreAnnotations.SentencesAnnotation.class);
+
+        for(CoreMap sentence: sentences) {
+            // traversing the words in the current sentence
+            // a CoreLabel is a CoreMap with additional token-specific methods
+            for (CoreLabel token : sentence.get(CoreAnnotations.TokensAnnotation.class)) {
+                // this is the text of the token
+                String word = token.get(CoreAnnotations.TextAnnotation.class);
+                // this is the POS tag of the token
+                String pos = token.get(CoreAnnotations.PartOfSpeechAnnotation.class);
+                // this is the NER label of the token
+                String ne = token.get(CoreAnnotations.NamedEntityTagAnnotation.class);
+                System.out.println("word " + word + " ,pos: " + pos + " ,ne: " + ne);
+            }
+
+        }
 
     }
 }
