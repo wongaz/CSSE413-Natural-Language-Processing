40d76654bba2f2edd552b0413a2c8aa5365dbf1e
Alex <wongaz@rose-hulman.edu>
2017-11-06 20:50:24 -0500

    Damn All White Space

--- src/main/java/NLP2.java
+++ src/main/java/NLP2.java
@@ -1,8 +1,15 @@
 import edu.stanford.nlp.ling.CoreAnnotations;
 import edu.stanford.nlp.ling.CoreLabel;
+import edu.stanford.nlp.ling.IndexedWord;
 import edu.stanford.nlp.pipeline.Annotation;
 import edu.stanford.nlp.pipeline.StanfordCoreNLP;
+import edu.stanford.nlp.semgraph.SemanticGraph;
+import edu.stanford.nlp.semgraph.SemanticGraphCoreAnnotations;
+import edu.stanford.nlp.trees.GrammaticalRelation;
+import edu.stanford.nlp.trees.Tree;
+import edu.stanford.nlp.trees.TreeCoreAnnotations;
 import edu.stanford.nlp.util.CoreMap;
+import edu.stanford.nlp.util.Pair;
 
 import java.util.List;
 import java.util.Properties;
@@ -26,7 +33,9 @@ public class NLP2 {
 
         StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
         // read some text in the text variable
-        String text = "Pick up that block";
+        //String text = "Pick up that block";
+        String text = "\"I voted for Nader because he was most aligned with my values, \" she said.";
+        //String text = "In 1921, Einstein received the Nobel Prize for his original work on the photoelectric effect.";
         Annotation document = new Annotation(text);
         pipeline.annotate(document);
         System.out.println(text.toString());
@@ -44,8 +53,70 @@ public class NLP2 {
                 String ne = token.get(CoreAnnotations.NamedEntityTagAnnotation.class);
                 System.out.println("word " + word + " ,pos: " + pos + " ,ne: " + ne);
             }
+            //this is the parse tree of the current sentence
+//
+
+            SemanticGraph dependencies = sentence.get(SemanticGraphCoreAnnotations.CollapsedCCProcessedDependenciesAnnotation.class);
+            System.out.println();
+            System.out.println(dependencies);
+
+            IndexedWord root = dependencies.getFirstRoot();
+            // type of root
+            String type = root.tag();
+            switch (type) {
+                case "VB": processVerbPhrase(dependencies, root); break;
+                case "NN": processNounPhrase(dependencies, root); break;
+                case "DT": processDeterminer(dependencies, root); break;
+                default: System.out.println("Cannot identify sentence structure.");
+            }
+            IndexedWord subject = null;
+            String quantifier = null;
+            Boolean goodToGo = false;
+            Boolean TopLevelNegation = false;
+            Boolean PredicateLevelNegation = false;
+
+            //List<Pair<GrammaticalRelation,IndexedWord>> s = dependencies.childPairs(predicate);
+//            for (Pair<GrammaticalRelation,IndexedWord> item : s) {
+//        	    if (item.first.toString().equals("nsubj")) {
+//        		  subject = item.second;
+//                }
+//        	    if (item.first.toString().equals("cop") && (item.second.originalText().toLowerCase().equals("are") || item.second.originalText().toLowerCase().equals("is"))) {
+//        		    goodToGo = true;
+//                }
+//        	    if (item.first.toString().equals("neg")) {
+//        	        PredicateLevelNegation = true;
+//        	    }
+//            }
 
         }
 
+
+    }
+    // Processes: {This, that} one?
+    static public void processDeterminer(SemanticGraph dependencies, IndexedWord root){
+        List<Pair<GrammaticalRelation,IndexedWord>> s = dependencies.childPairs(root);
+
+        System.out.println("Identity of object: " + root.originalText().toLowerCase());
+    }
+
+    //Processes: {That, this, the} {block, sphere}
+    static public void processNounPhrase(SemanticGraph dependencies, IndexedWord root){
+        List<Pair<GrammaticalRelation,IndexedWord>> s = dependencies.childPairs(root);
+
+        System.out.println("Identity of object: " + root.originalText().toLowerCase());
+        System.out.println("Type of object: " + s.get(0).second.originalText().toLowerCase());
+    }
+
+    // Processes: {Pick up, put down} {that, this} {block, sphere}
+    static public void processVerbPhrase(SemanticGraph dependencies, IndexedWord root){
+        List<Pair<GrammaticalRelation,IndexedWord>> s = dependencies.childPairs(root);
+        Pair<GrammaticalRelation,IndexedWord> prt = s.get(0);
+        Pair<GrammaticalRelation,IndexedWord> dobj = s.get(1);
+
+        List<Pair<GrammaticalRelation,IndexedWord>> newS = dependencies.childPairs(dobj.second);
+
+        System.out.println("Action: " + root.originalText().toLowerCase() + prt.second.originalText().toLowerCase());
+        System.out.println("Type of object: " + dobj.second.originalText().toLowerCase());
+        System.out.println("Identity of object: " + newS.get(0).second.originalText().toLowerCase());
     }
 }
